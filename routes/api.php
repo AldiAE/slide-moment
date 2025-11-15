354befb57c6fe3f65699cfb4a176d2d1fef50c8c
AldiAE <aldirosyad68@gmail.com>
2025-11-15 14:46:53 +0700

    modified api.php

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\SectionController;
-use App\Http\Controllers\RowController; 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::apiResource('pages', PageController::class);
-Route::apiResource('sections', SectionController::class);
-Route::apiResource('rows', RowController::class);
